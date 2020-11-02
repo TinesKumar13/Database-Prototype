d68f43181cd738dc4f9c600f4e468774c57e89c2
TinesKumar13 <tineshreds@gmail.com>
2020-11-02 15:54:46 +0800

    Version 4

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use("/api", postsRoute)
 app.use("/api/user", authRoute)
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname, "client", "build")))
+    app.use(express.static("client/build"))
     app.get('*', (req, res) => {
         res.sendFile(path.join(__dirname, 'client', 'build' , 'index.html'));
     })
